4749682a45edb90349ddcfd9c12e6fa909077293
Orlando <jiax.xie@foxmail.com>
2025-01-14 02:39:52 -0600

    go包管理

--- go.mod
+++ go.mod
@@ -2,4 +2,3 @@ module demo1
 
 go 1.23
 
-require github.com/Jeffail/tunny v0.1.4 // indirect
--- main.go
+++ main.go
@@ -2,10 +2,8 @@ package main
 
 import (
 	"fmt"
-	"github.com/Jeffail/tunny"
 )
 
 func main() {
 	fmt.Println("hello world!")
-	tunny.Pool{}
 }
