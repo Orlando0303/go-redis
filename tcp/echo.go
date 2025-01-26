e305812dfdac5939be0aa6125344354914babba7
Orlando <jiax.xie@foxmail.com>
2025-01-25 18:31:00 -0600

    go-TCP-EchoHandler EchoClient

--- tcp/echo.go
+++ tcp/echo.go
@@ -1,22 +1,25 @@
 package tcp
 
 import (
+	"bufio"
 	"context"
+	"go-redis/lib/logger"
 	"go-redis/lib/sync/atomic"
 	"go-redis/lib/sync/wait"
+	"io"
 	"net"
 	"sync"
 	"time"
 )
 
 type EchoClient struct {
-	conn    net.Conn
-	waiting wait.Wait
+	Conn    net.Conn
+	Waiting wait.Wait
 }
 
 func (client *EchoClient) close() error {
-	client.waiting.WaitWithTimeout(10 * time.Second)
-	client.conn.Close()
+	client.Waiting.WaitWithTimeout(10 * time.Second)
+	client.Conn.Close()
 	return nil
 }
 
@@ -29,11 +32,35 @@ func (handler *EchoHandler) Handle(ctx context.Context, conn net.Conn) {
 	if handler.closing.Get() {
 		_ = conn.Close()
 	}
-	client := &EchoClient{conn: conn}
+	client := &EchoClient{Conn: conn}
 	handler.activeConn.Store(client, struct{}{})
+	reader := bufio.NewReader(conn)
+	for {
+		msg, err := reader.ReadString('\n')
+		if err != nil {
+			if err == io.EOF {
+				logger.Info("Connection close")
+				handler.activeConn.Delete(client)
+			} else {
+				logger.Warn(err)
+			}
+			return
+		}
+
+		client.Waiting.Add(1)
+		_, _ = conn.Write([]byte(msg))
+		client.Waiting.Done()
+	}
+
 }
 
 func (handler *EchoHandler) Close() error {
-	//TODO implement me
-	panic("implement me")
+	logger.Info("Handler shutting down")
+	handler.closing.Set(true)
+	handler.activeConn.Range(func(key, value any) bool {
+		client := key.(*EchoClient)
+		_ = client.Conn.Close()
+		return true
+	})
+	return nil
 }
