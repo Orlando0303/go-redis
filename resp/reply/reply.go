f63d0eb5ca32c07c0a6df07df762d6ea89420735
Orlando <jiax.xie@foxmail.com>
2025-01-26 23:27:54 -0600

    go-resp-自定义reply

---
+++ resp/reply/reply.go
@@ -0,0 +1,120 @@
+package reply
+
+import (
+	"bytes"
+	"go-redis/interface/resp"
+	"strconv"
+)
+
+var ( // CRLF is the line separator of redis serialization protocol
+	CRLF = "\r\n"
+)
+
+/* ---- Bulk Reply ---- */
+
+// BulkReply stores a binary-safe string
+type BulkReply struct {
+	Arg []byte
+}
+
+func (r *BulkReply) ToBytes() []byte {
+	if len(r.Arg) == 0 {
+		return []byte("$-1" + CRLF)
+	}
+	return []byte("$" + strconv.Itoa(len(r.Arg)) + CRLF + string(r.Arg) + CRLF)
+}
+
+func MakeBulkReply(arg []byte) *BulkReply {
+	return &BulkReply{Arg: arg}
+}
+
+/* ---- Multi Bulk Reply ---- */
+
+// MultiBulkReply stores a list of string
+type MultiBulkReply struct {
+	Arg [][]byte
+}
+
+func (r *MultiBulkReply) ToBytes() []byte {
+	argLen := len(r.Arg)
+	var buf bytes.Buffer
+	buf.WriteString("*" + strconv.Itoa(argLen) + CRLF)
+	for _, arg := range r.Arg {
+		if arg == nil {
+			buf.WriteString("$-1" + CRLF)
+		} else {
+			buf.WriteString("$" + strconv.Itoa(len(arg)) + CRLF + string(arg) + CRLF)
+		}
+	}
+	return buf.Bytes()
+}
+
+func MakeMultiBulkReply(args [][]byte) *MultiBulkReply {
+	return &MultiBulkReply{Arg: args}
+}
+
+/* ---- Status Reply ---- */
+
+// StatusReply stores a simple status string
+type StatusReply struct {
+	Status string
+}
+
+// ToBytes marshal redis.Reply
+func (r *StatusReply) ToBytes() []byte {
+	return []byte("+" + r.Status + CRLF)
+}
+
+// MakeStatusReply creates StatusReply
+func MakeStatusReply(status string) *StatusReply {
+	return &StatusReply{
+		Status: status,
+	}
+}
+
+/* ---- Int Reply ---- */
+
+// IntReply stores an int64 number
+type IntReply struct {
+	Code int64
+}
+
+// ToBytes marshal redis.Reply
+func (r *IntReply) ToBytes() []byte {
+	return []byte(":" + strconv.FormatInt(r.Code, 10) + CRLF)
+}
+
+// MakeIntReply creates int reply
+func MakeIntReply(code int64) *IntReply {
+	return &IntReply{
+		Code: code,
+	}
+}
+
+/* ---- Error Reply ---- */
+
+// StandardErrReply represents handler error
+type StandardErrReply struct {
+	Status string
+}
+
+// ToBytes marshal redis.Reply
+func (r *StandardErrReply) ToBytes() []byte {
+	return []byte("-" + r.Status + CRLF)
+}
+
+func (r *StandardErrReply) Error() string {
+	return r.Status
+}
+
+// MakeErrReply creates StandardErrReply
+func MakeErrReply(status string) *StandardErrReply {
+	return &StandardErrReply{
+		Status: status,
+	}
+}
+
+// IsErrorReply returns true if the given reply is error
+func IsErrorReply(reply resp.Reply) bool {
+	return reply.ToBytes()[0] == '-'
+}
